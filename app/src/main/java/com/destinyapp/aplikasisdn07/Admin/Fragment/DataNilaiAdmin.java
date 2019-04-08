524bac1b4acc62f3624fd752e7e3c3338493b45f
Muhammad Supriyadi <musupadi159@gmail.com>
2019-04-08 19:07:38 +0700

    Terusan dari Laptop

---
+++ app/src/main/java/com/destinyapp/aplikasisdn07/Admin/Fragment/DataNilaiAdmin.java
@@ -0,0 +1,30 @@
+package com.destinyapp.aplikasisdn07.Admin.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.destinyapp.aplikasisdn07.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DataNilaiAdmin extends Fragment {
+
+
+    public DataNilaiAdmin() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_data_nilai_admin, container, false);
+    }
+
+}
